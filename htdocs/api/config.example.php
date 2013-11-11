fca625c15404c896d6c0682575d7afdb5fdcbd0d
Cameron King <cking@ecc12.com>
2013-11-11 01:11:54 +0000

    move api config to separate file

---
+++ htdocs/api/config.example.php
@@ -0,0 +1,15 @@
+<?php
+
+global $APICONFIG;
+$APICONFIG = array(
+  "db" => array(
+    "host" => "db01.server.com",
+    "user" => "churchinfo_api",
+    "password" => "bnsdfahln23rnl32",
+    "dbname" => "churchinfo"
+  ),
+  "keys" => array(
+    "0" => "7nyon7osdfan7osdan"
+  )
+);
+
