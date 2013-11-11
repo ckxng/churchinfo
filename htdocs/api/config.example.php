71f68b2411939cb6c1211d2a43fe760ec7623537
Cameron King <cking@ecc12.com>
2013-11-11 01:47:58 +0000

    removing useless global and clarifying example

--- htdocs/api/config.example.php
+++ htdocs/api/config.example.php
@@ -1,6 +1,5 @@
 <?php
 
-global $APICONFIG;
 $APICONFIG = array(
   "db" => array(
     "host" => "db01.server.com",
@@ -9,7 +8,7 @@ $APICONFIG = array(
     "dbname" => "churchinfo"
   ),
   "keys" => array(
-    "0" => "7nyon7osdfan7osdan"
+    "web1" => "7nyon7osdfan7osdan"
   )
 );
 
